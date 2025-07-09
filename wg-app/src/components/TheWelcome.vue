a0adf8e443027fec53c9cac14f614c35307c3522
Till Voss <till.voss@alumni.fh-aachen.de>
2025-07-09 13:05:52 +0200

    testForDeployment

--- wg-app/src/components/TheWelcome.vue
+++ wg-app/src/components/TheWelcome.vue
@@ -16,7 +16,7 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
     </template>
     <template #heading>Documentation</template>
 
-    Vue’s
+    Vue’s swaggo
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
     provides you with all information you need to get started.
   </WelcomeItem>
